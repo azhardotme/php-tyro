b49c029c0c7ac740a76a6fda1fc568db79aec59d
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2022-06-13 01:59:42 +0600

    add some functions

--- task-1/builtinfunction.php
+++ task-1/builtinfunction.php
@@ -1,2 +1,5 @@
 <?php
-echo "PHP Built in functions!";
+echo "PHP Built in functions!<br>";
+
+echo '0:    ' . (boolval(0)  ? 'true' : 'false') . "<br>";
+echo '1:    ' . (boolval(1)  ? 'true' : 'false') . "<br>";
