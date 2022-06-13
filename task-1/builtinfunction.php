dd4be026b2de3aea4a122f10825a2fe4fa5d467e
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2022-06-13 17:08:52 +0600

    add boolval()

--- task-1/builtinfunction.php
+++ task-1/builtinfunction.php
@@ -1,5 +1,8 @@
 <?php
-echo "PHP Built in functions!<br>";
+echo "<h2>PHP Built in functions!</h2><br>";
 
+#boolval()
 echo '0:    ' . (boolval(0)  ? 'true' : 'false') . "<br>";
 echo '1:    ' . (boolval(1)  ? 'true' : 'false') . "<br>";
+echo '0.0:    ' . (boolval(0.0)  ? 'true' : 'false') . "<br>";
+echo '"Hello": ' . (boolval("Hello") ? 'true' : 'false') . "<br>";
