b0d7afafb7968aea59e42f082f07949403efe372
azhardotme <azharcse1997@gmail.com>
2022-06-20 13:51:09 +0600

    added math function

---
+++ task-3/math.php
@@ -0,0 +1,46 @@
+<?php
+#abs()
+echo abs(-4.2) . "<br>";
+echo abs(5). "<br>";
+echo abs(-6) . "<br>";
+
+#ceil()
+
+echo ceil(4.3) ."<br>";
+echo ceil(9.999) ."<br>";
+echo ceil(-3.14) ."<br>";
+
+#floor()
+echo floor(3.4) . "<br>";
+echo floor(-3.4) . "<br>";
+echo floor(3.99) . "<br>";
+
+#round()
+echo round(3.4) . "<br>";
+echo round(3.6) . "<br>";
+echo round(-3.6) . "<br>";
+
+#max()
+echo max(2,7) . "<br>";
+echo max(2,-7) . "<br>";
+echo max(-2,-7) . "<br>";
+
+#min()
+echo min(9,5) . "<br>";
+echo min(9,-5) . "<br>";
+
+#pow()
+echo pow(2,3) . "<br>";
+echo pow(2,4) . "<br>";
+
+#sqrt()
+echo sqrt(2) . "<br>";
+echo sqrt(16) . "<br>";
+
+#pi()
+echo pi() . "<br>";
+echo M_PI . "<br>";
+
+#count()
+
+echo count(2,5,4) . "<br>";
